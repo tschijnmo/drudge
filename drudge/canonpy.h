7a51767ccc076fbed09a0325b8cb0fd3d86aaa90
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-01-21 16:48:05 -0600

    Add files for canonpy
    The header file, C++ source file, and the Python typing interface file
    are all added.

---
+++ drudge/canonpy.h
@@ -0,0 +1,7 @@
+/* vim: set filetype=cpp: */
+
+/** Canonpy, a simple wrapper for libcanon in Python.
+ *
+ * This is a simple wrapper over some of the facilities in libcanon to be used
+ * in Python.  It directly targets what is needed for drudge.
+ */
---
+++ drudge/canonpy.pyi
@@ -0,0 +1,8 @@
+"""
+Canonpy, a simple wrapper of libcanon for Python
+================================================
+
+This is the interface file for the classes and functions in canonpy.  Note that
+this file is more intended for tooling than for documentation purpose.
+
+"""
