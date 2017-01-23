1a400264a7c5902b2e2f333ef29ad2d76d4d4431
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-01-23 15:36:20 -0600

    Export Perm class to drudge base
    Different from PySLATA, drudge is going to have a shallow namespace
    design.  All public symbols are going to be put in the base package.

---
+++ drudge/__init__.py
@@ -0,0 +1,11 @@
+"""
+Drudge, a symbolic system for non-commutative and tensor algebra
+================================================================
+
+"""
+
+from .canonpy import Perm
+
+__all__ = [
+    'Perm',
+]
