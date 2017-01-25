ad18f0719778066a39030cba846ea0614299e390
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-01-25 13:25:24 -0600

    Forward Vec class to base drudge module

--- drudge/__init__.py
+++ drudge/__init__.py
@@ -5,9 +5,14 @@ Drudge, a symbolic system for non-commutative and tensor algebra
 """
 
 from .canonpy import Perm, Group, canon_eldag
+from .vec import Vec
 
 __all__ = [
+    # Canonpy.
     'Perm',
     'Group',
     'canon_eldag',
+
+    # Vec.
+    'Vec'
 ]
