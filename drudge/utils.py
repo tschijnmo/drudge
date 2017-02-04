9d61b35685e4bbb007d7c9a28ee68765e252b832
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-02-03 23:29:30 -0600

    Add class and metaclass for concrete symbols
    The resulted class is convenient for defining enumeration types whose
    values are SymPy expressions, for example, for creation or annihilation,
    spin up and down.

--- drudge/utils.py
+++ drudge/utils.py
@@ -3,8 +3,11 @@
 from collections.abc import Sequence
 
 from pyspark import SparkContext
-from sympy import (sympify, Symbol, Expr, SympifyError, count_ops,
-                   default_sort_key)
+from sympy import (
+    sympify, Symbol, Expr, SympifyError, count_ops,
+    default_sort_key, AtomicExpr, Integer, S
+)
+from sympy.core.assumptions import ManagedProperties
 
 
 #
@@ -59,6 +62,137 @@ def is_higher(obj, priority):
     """
 
     return getattr(obj, '_op_priority', priority - 1) > priority
+
+
+class _ConcrSymbsMeta(ManagedProperties):
+    """The meta class for concrete symbols.
+
+    The primary purpose of this metaclass is to set the concrete singleton
+    values from the symbols set in the class body.
+    """
+
+    SYMBS_INPUT = '_symbs_'
+
+    def __new__(mcs, name, bases, attrs):
+        """Create the new concrete symbols class."""
+
+        cls = super().__new__(mcs, name, bases, attrs)
+
+        if not hasattr(cls, mcs.SYMBS_INPUT):
+            raise AttributeError('Cannot find attribute ' + mcs.SYMBS_INPUT)
+
+        symbs = getattr(cls, mcs.SYMBS_INPUT)
+        if symbs is None:
+            # Base class.
+            return cls
+
+        if not isinstance(symbs, Sequence):
+            raise ValueError('Invalid symbols', symbs, 'expecting a sequence')
+        for i in symbs:
+            invalid = not isinstance(i, Sequence) or len(i) != 2 or any(
+                not isinstance(j, str) for j in i
+            )
+            if invalid:
+                raise ValueError(
+                    'Invalid symbol', i,
+                    'expecting pairs of identifier and LaTeX form.'
+                )
+        if len(symbs) < 2:
+            raise ValueError(
+                'Invalid symbols ', symbs, 'expecting multiple of them'
+            )
+
+        for i, v in enumerate(symbs):
+            obj = cls(i)
+            setattr(cls, v[0], obj)
+            continue
+
+        return cls
+
+
+class ConcrSymbs(AtomicExpr, metaclass=_ConcrSymbsMeta):
+    """Base class for concrete symbols.
+
+    Subclasses can set `_symbs_` inside the class body to be a sequence of
+    string pairs.  Then attributes named after the first field of the pairs will
+    be created, with the LaTeX form controlled by the second pair.
+
+    The resulted values are valid SymPy expressions.  They are ordered according
+    to their order in the given sequence.
+
+    """
+
+    _symbs_ = None
+
+    _VAL_FIELD = '_val_index'
+    __slots__ = [_VAL_FIELD]
+
+    def __init__(self, val_index):
+        """Initialize the concrete symbol object.
+        """
+        if self._symbs_ is None:
+            raise ValueError('Base ConcrSymbs class cannot be instantiated')
+        setattr(self, self._VAL_FIELD, val_index)
+
+    @property
+    def args(self):
+        """The argument for SymPy."""
+        return Integer(getattr(self, self._VAL_FIELD)),
+
+    def __str__(self):
+        """Get the string representation of the symbol."""
+        return self._symbs_[getattr(self, self._VAL_FIELD)][0]
+
+    def __repr__(self):
+        """Get the machine readable string representation."""
+        return '.'.join([type(self).__name__, str(self)])
+
+    _op_priority = 20.0
+
+    def __eq__(self, other):
+        """Test two values for equality."""
+        return isinstance(other, type(self)) and self.args == other.args
+
+    def __hash__(self):
+        """Hash the concrete symbol object."""
+        return hash(repr(self))
+
+    def __lt__(self, other):
+        """Test two values for less than order.
+
+        The order will be based on the order given in the class.
+        """
+        return self.args < other.args
+
+    def __gt__(self, other):
+        """Test two values for greater than."""
+        return self.args > other.args
+
+    def __sub__(self, other):
+        """Subtract the current value with another.
+
+        This method is mainly to be able to work together with the Kronecker
+        delta class from SymPy.
+        """
+
+        if not isinstance(other, type(self)):
+            raise ValueError(
+                'Invalid operation for ', (self, other),
+                'concrete symbols can only be subtracted for the same type'
+            )
+
+        return self.args[0] - other.args[0]
+
+    def sort_key(self, order=None):
+        return (
+            self.class_key(),
+            (1, tuple(i.sort_key() for i in self.args)),
+            S.One.sort_key(), S.One
+        )
+
+    def _latex(self, _):
+        """Print itself as LaTeX code."""
+        return self._symbs_[self.args[0]][1]
 
 
 #
