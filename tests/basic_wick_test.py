e687708ec42dad844ac76aa55af7d5fb9027edb2
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-02-03 23:32:38 -0600

    Add tests for cr/an character singleton values
    More importantly, this test also serves to test the concrete symbols
    class and metaclass from the utilities module.

--- tests/basic_wick_test.py
+++ tests/basic_wick_test.py
@@ -4,10 +4,13 @@ The tests in the module is attempted to test the core Wick facility on terms,
 without parallelization by Spark.
 """
 
-from sympy import symbols, IndexedBase
+import pickle
+
+from sympy import symbols, IndexedBase, latex, KroneckerDelta
 
 from drudge import Range, Vec, CR, AN, FERMI, FockDrudge
 from drudge.term import sum_term
+from drudge.utils import sympy_key
 from drudge.wick import wick_expand
 
 
@@ -41,9 +44,9 @@ def test_wick_expansion_of_term(spark_ctx):
     dumms = {r: [a, b, c, d]}
 
     res = {
-        i.simplify_deltas([lambda x: r])
-            .canon(vec_colour=dr.vec_colour)
-            .reset_dumms(dumms)[0]
+        i.simplify_deltas([lambda x: r]).canon(
+            vec_colour=dr.vec_colour
+        ).reset_dumms(dumms)[0]
         for i in res
         }
 
@@ -59,3 +62,44 @@ def test_wick_expansion_of_term(spark_ctx):
     }
 
     assert (res == expected)
+
+
+def test_ancr_character_has_basic_properties():
+    """Test the annihilation and creation character singleton values.
+
+    This test case also serves as the test for the general concrete symbol
+    utility class and meta-class.
+    """
+
+    # Its objects will be serialized and deserialized.
+    n_cr, n_an = [pickle.loads(pickle.dumps(i)) for i in [CR, AN]]
+
+    # We test both the original value and the deserialized values.
+    for cr, an in [(CR, AN), (n_cr, n_an)]:
+        # Printing, all kinds of printing.
+        assert str(cr) == 'CR'
+        assert str(an) == 'AN'
+        assert repr(cr) == '_OpChar.CR'
+        assert repr(an) == '_OpChar.AN'
+        assert latex(cr) == r'\dag'
+        assert latex(an) == ''
+
+        # Ordering, in its original form and as SymPy key.
+        assert cr == cr
+        assert an == an
+        assert cr < an
+        assert not cr > an
+        assert sympy_key(cr) == sympy_key(cr)
+        assert sympy_key(an) == sympy_key(an)
+        assert sympy_key(cr) < sympy_key(an)
+        assert not sympy_key(cr) > sympy_key(an)
+
+        # Subtraction, purpose is the handling in deltas.
+        assert cr - cr == 0
+        assert an - an == 0
+        assert cr - an != 0
+        assert an - cr != 0
+        assert KroneckerDelta(cr, cr) == 1
+        assert KroneckerDelta(an, an) == 1
+        assert KroneckerDelta(cr, an) == 0
+        assert KroneckerDelta(an, cr) == 0
