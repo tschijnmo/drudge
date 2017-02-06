85b75c65bb6d4878919f1a2b8a0facf7fde9748b
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-02-05 23:16:02 -0600

    Add option to use dummy spark in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,21 @@
 """Project-wide shared test fixtures."""
 
+import os
+
 import pytest
-from pyspark import SparkConf, SparkContext
 
 
 @pytest.fixture(scope='session', autouse=True)
 def spark_ctx():
     """A simple spark context."""
 
-    conf = SparkConf().setMaster('local[2]').setAppName('drudge-unittest')
-    ctx = SparkContext(conf=conf)
+    if 'DUMMY_SPARK' in os.environ:
+        from dummy_spark import SparkConf, SparkContext
+        conf = SparkConf()
+        ctx = SparkContext(master='', conf=conf)
+    else:
+        from pyspark import SparkConf, SparkContext
+        conf = SparkConf().setMaster('local[2]').setAppName('drudge-unittest')
+        ctx = SparkContext(conf=conf)
 
     return ctx
