207c3fc8467c7f216e06b88f87433dc4eb46e13c
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-02-15 22:14:36 -0600

    Fix name injection test for the new Drudge update

--- tests/name_injection_test.py
+++ tests/name_injection_test.py
@@ -1,12 +1,16 @@
 """Test for the name inject utility."""
 
+import types
+
 from drudge import Drudge
 
 
 def test_drudge_injects_names():
     """Test the name injection method of drudge."""
 
-    dr = Drudge(None)  # Dummy drudge.
+    # Dummy drudge.
+    dr = Drudge(types.SimpleNamespace(defaultParallelism=1))
+
     string_name = 'string_name'
     dr.set_name(string_name)
     dr.set_name(1, 'one')
