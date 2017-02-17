f8be702b2bc4a084c1ae15896940790de29b02e0
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-02-17 13:46:33 -0600

    Add napoleon extension to Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,9 @@
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
