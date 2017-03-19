476e5ea065a912ef391a47faee4acceeec1465f7
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-03-18 21:47:13 -0500

    Reorganize vectors for su2 drudge
    Now we do not use a single vector for shift operators.  This removed the
    need for characters and really simplifies things.

--- drudge/su2.py
+++ drudge/su2.py
@@ -7,25 +7,7 @@ from sympy import Integer
 
 from .genquad import GenQuadDrudge
 from .term import Vec
-from .utils import EnumSymbs, sympy_key
-
-
-class RaisingLoweringChar(EnumSymbs):
-    """Raising/lowering characters of ladder operators.
-
-    Its values, which can be accessed as the class attributes ``RAISE`` and
-    ``LOWER`` are also forwarded to module scope.  They should be used as the
-    first index to vectors representing spin ladder operators.
-    """
-
-    _symbs_ = [
-        ('RAISE', '+'),
-        ('LOWER', '-')
-    ]
-
-
-RAISE = RaisingLoweringChar.RAISE
-LOWER = RaisingLoweringChar.LOWER
+from .utils import sympy_key
 
 
 class SU2LatticeDrudge(GenQuadDrudge):
@@ -37,12 +19,12 @@ class SU2LatticeDrudge(GenQuadDrudge):
     commute.
 
     The the normal-ordering operation would try to put raising operators before
-    the Cartan operators, which comes before the lowering operators.
+    the Cartan operators, which come before the lowering operators.
 
     """
 
     def __init__(
-            self, ctx, cartan_label='J^z', shift_label='J',
+            self, ctx, cartan=Vec('J^z'), raise_=Vec('J^+'), lower=Vec('J^-'),
             root=Integer(1), norm=Integer(2),
             **kwargs
     ):
@@ -54,15 +36,18 @@ class SU2LatticeDrudge(GenQuadDrudge):
         ctx
             The Spark context for the drudge.
 
-        cartan_label
-            The label for the basis operator in the Cartan subalgebra
-            (:math:`J^z` operator).  It is registered in the name archive by the
-            first letter followed by an underscore.
+        cartan
+            The basis operator for the Cartan subalgebra (:math:`J^z` operator
+            for spin problem).  It is registered in the name archive by the
+            first letter in its label followed by an underscore.
 
-        shift_label
-            The label for the shift (raising and lowering) operators.  They are
-            also registered in the name archive by this label followed by ``_p``
-            and ``_m``.
+        raise_
+            The raising operator.  It is also also registered in the name
+            archive by the first letter in its label followed by ``_p``.
+
+        lower
+            The lowering operator, registered by the first letter followed by
+            ``_m``.
 
         root
             The coefficient for the commutator between the Cartan and shift
@@ -78,25 +63,21 @@ class SU2LatticeDrudge(GenQuadDrudge):
         """
         super().__init__(ctx, **kwargs)
 
-        cartan = Vec(cartan_label)
         self.cartan = cartan
-        shift = Vec(shift_label)
-        self.shift = shift
-        raise_ = shift[RAISE]
         self.raise_ = raise_
-        lower = shift[LOWER]
         self.lower = lower
         self.set_name(**{
-            cartan_label[0] + '_': cartan,
-            shift_label + '_p': raise_,
-            shift_label + '_m': lower
+            cartan.label[0] + '_': cartan,
+            raise_.label[0] + '_p': raise_,
+            lower.label[0] + '_m': lower
         })
 
         self._root = root
         self._norm = norm
 
         self._swapper = functools.partial(
-            _swap_su2, cartan=cartan, shift=shift, root=root, norm=norm
+            _swap_su2, cartan=cartan, raise_=raise_, lower=lower,
+            root=root, norm=norm
         )
 
     def swapper(self) -> GenQuadDrudge.Swapper:
@@ -104,20 +85,20 @@ class SU2LatticeDrudge(GenQuadDrudge):
         return self._swapper
 
 
-def _swap_su2(vec1: Vec, vec2: Vec, *, cartan, shift, root, norm):
+def _swap_su2(
+        vec1: Vec, vec2: Vec, *,
+        cartan, raise_, lower, root, norm
+):
     """Swap two vectors based on the SU2 rules.
     """
 
-    char1, indice1 = _parse_vec(vec1, cartan, shift)
-    char2, indice2 = _parse_vec(vec2, cartan, shift)
+    char1, indice1, key1 = _parse_vec(vec1, cartan, raise_, lower)
+    char2, indice2, key2 = _parse_vec(vec2, cartan, raise_, lower)
     if len(indice1) != len(indice2):
         raise ValueError(
             'Invalid SU2 generators on lattice', (vec1, vec2),
             'incompatible number of lattice indices'
         )
-
-    key1 = [sympy_key(i) for i in indice1]
-    key2 = [sympy_key(i) for i in indice2]
     delta = functools.reduce(
         operator.mul, zip(indice1, indice2), _UNITY
     )
@@ -169,34 +150,24 @@ _CARTAN = 1
 _LOWER = 2
 
 
-def _parse_vec(vec, cartan, shift):
-    """Get the character and lattice indices of the vector.
+def _parse_vec(vec, cartan, raise_, lower):
+    """Get the character and lattice indices, and indices keys of the vector.
     """
 
-    if vec.base == cartan:
-        return _CARTAN, vec.indices
-    elif vec.base == shift:
-
-        if len(vec.indices) < 1:
-            raise ValueError(
-                'Invalid shift operator for SU2 algebra', vec,
-                'expecting raising/lowering character'
-            )
-
-        char_index = vec.indices[0]
-        if char_index == RAISE:
-            char = _RAISE
-        elif char_index == LOWER:
-            char = _LOWER
-        else:
-            raise ValueError(
-                'Invalid shift character', char_index, 'on vector', vec
-            )
-
-        return char, vec.indices[1:]
-
+    base = vec.base
+    if base == cartan:
+        char = _CARTAN
+    elif base == raise_:
+        char = _RAISE
+    elif base == lower:
+        char = _LOWER
     else:
         raise ValueError('Unexpected vector for SU2 algebra', vec)
+
+    indices = vec.indices
+    keys = tuple(sympy_key(i) for i in indices)
+
+    return char, indices, keys
 
 
 _UNITY = Integer(1)
