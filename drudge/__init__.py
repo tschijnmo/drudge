92f6257baf061d647fb6178497e459a7ea81aaa2
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-03-20 23:21:17 -0500

    Bump version to 0.3.0

--- drudge/__init__.py
+++ drudge/__init__.py
@@ -18,7 +18,7 @@ from .su2 import SU2LatticeDrudge
 from .clifford import CliffordDrudge, inner_by_delta
 from .utils import sum_, prod_, Stopwatch
 
-__version__ = '0.3.0dev'
+__version__ = '0.3.0'
 
 __all__ = [
     # Canonpy.
