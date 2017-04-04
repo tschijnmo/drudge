ae0f7d610a4b31346ff496c93d3d73f7a314c55b
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-04-03 19:40:01 -0500

    Add test for restricted particle-hole drudge
    Here the original Hamiltonian is compared with the Hamiltonian written
    in terms of the UGA generators.

--- tests/spin_one_half_test.py
+++ tests/spin_one_half_test.py
@@ -1,11 +1,11 @@
 """Tests for the drudges with explicit one-half spin."""
 
 import pytest
-
-from sympy import IndexedBase, symbols, Rational
+from sympy import IndexedBase, symbols, Rational, KroneckerDelta
 
 from drudge import (
-    CR, AN, UP, DOWN, SpinOneHalfGenDrudge, SpinOneHalfPartHoleDrudge
+    CR, AN, UP, DOWN, SpinOneHalfGenDrudge, SpinOneHalfPartHoleDrudge,
+    RestrictedPartHoleDrudge
 )
 
 
@@ -86,3 +86,37 @@ def test_spin_one_half_particle_hole_drudge_has_basic_properties(parthole):
     assert len([i for i in ham_terms if len(i.vecs) == 2]) == 8
     assert len([i for i in ham_terms if len(i.vecs) == 4]) == 36
     assert dr.ham.n_terms == 8 + 36
+
+
+@pytest.fixture(scope='module')
+def restricted_parthole(spark_ctx):
+    """The fixture with a restricted particle-hole drudge."""
+    return RestrictedPartHoleDrudge(spark_ctx)
+
+
+def test_restricted_parthole_drudge_has_good_hamiltonian(restricted_parthole):
+    """Test Hamiltonian of restricted particle-hole drudge.
+
+    Here the original Hamiltonian is going to be compared with the Hamiltonian
+    written in terms of the unitary group generators.
+    """
+
+    dr = restricted_parthole
+    p = dr.names
+
+    e_ = p.e_
+    h = dr.one_body
+    v = dr.two_body
+    half = Rational(1, 2)
+    orbs = tuple(dr.orb_ranges)
+    p, q, r, s = symbols('p q r s')
+
+    expected_ham = dr.sum(
+        (p, orbs), (q, orbs), h[p, q] * e_[p, q]
+    ) + dr.sum(
+        (p, orbs), (q, orbs), (r, orbs), (s, orbs),
+        half * v[p, r, q, s] * (
+            e_[p, q] * e_[r, s] - KroneckerDelta(q, r) * e_[p, s]
+        )
+    )
+    assert (dr.orig_ham - expected_ham).simplify() == 0
