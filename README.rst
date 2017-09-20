76e4a7b9adb06cd32b628d48a3345c0998e3f2a1
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-09-20 13:43:54 -0500

    Add travis CI badge to README.rst

--- README.rst
+++ README.rst
@@ -1,3 +1,7 @@
+.. image:: https://travis-ci.org/tschijnmo/drudge.svg?branch=master
+    :target: https://travis-ci.org/tschijnmo/drudge
+
+
 Drudge
 ------
 
--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.imgconverter'
     ]
 
 # Add any paths that contain templates here, relative to this directory.
