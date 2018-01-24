03cb33709a9d74b3b78b24bcd6121e9db031fe5c
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2018-01-24 17:33:21 -0600

    Add tests for the new range behaviour
    Here the new handling of bounds is concentrated.

--- tests/range_test.py
+++ tests/range_test.py
@@ -36,3 +36,35 @@ def test_range_has_basic_operations():
     assert symb0.upper is None
     assert len(symb0.args) == 1
     assert symb0.args[0] == symb1.label
+
+
+def test_bounds_operations_for_ranges():
+    """Properties about bounds for ranges.
+    """
+
+    l, u = sympify('l, u')
+    r = Range('R')
+    assert not r.bounded
+
+    magic = 10
+    d = {r: magic}
+    assert d[r] == magic
+
+    assert Range('S') not in d
+
+    # Add bounds should not change the identity of the range.
+    r_lu = r[l, u]
+    assert r_lu.bounded
+    assert r_lu.lower == l
+    assert r_lu.upper == u
+    assert d[r_lu] == 10
+
+    for r_02 in [
+        r_lu[0, 2],
+        r_lu.map(lambda x: x.xreplace({l: 0, u: 2}))
+    ]:
+        assert d[r_02] == 10
+        assert r_02.bounded
+        assert r_02.lower == 0
+        assert r_02.upper == 2
+        assert r_02.size == 2
